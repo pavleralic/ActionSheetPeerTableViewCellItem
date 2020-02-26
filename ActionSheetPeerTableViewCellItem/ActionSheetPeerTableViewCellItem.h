f01f188580d75600e0a23dca2690f2fc941ac0e8
Pavle Ralic <pavle.ralic@gmail.com>
2020-02-26 17:11:33 +0100

    Initial project setup

---
+++ ActionSheetPeerTableViewCellItem/ActionSheetPeerTableViewCellItem.h
@@ -0,0 +1,19 @@
+//
+//  ActionSheetPeerTableViewCellItem.h
+//  ActionSheetPeerTableViewCellItem
+//
+//  Created by Pavle Ralic on 2/26/20.
+//  Copyright © 2020 Pavle Ralic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ActionSheetPeerTableViewCellItem.
+FOUNDATION_EXPORT double ActionSheetPeerTableViewCellItemVersionNumber;
+
+//! Project version string for ActionSheetPeerTableViewCellItem.
+FOUNDATION_EXPORT const unsigned char ActionSheetPeerTableViewCellItemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionSheetPeerTableViewCellItem/PublicHeader.h>
+
+
