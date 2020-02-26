7171ad29b0a451415fd4bc69a4e78e6c8ba06dd3
Pavle Ralic <pavle.ralic@gmail.com>
2020-02-26 19:42:18 +0100

    Initial project setup

---
+++ ActionSheetPeerTableViewCellItem.podspec
@@ -0,0 +1,21 @@
+Pod::Spec.new do |spec|
+
+  spec.name         = "ActionSheetPeerTableViewCellItem"
+  spec.version      = "0.0.1"
+  spec.summary      = "A CocoaPods library written in Swift"
+
+  spec.description  = <<-DESC
+This CocoaPods library helps you perform calculation.
+                   DESC
+
+  spec.homepage     = "https://github.com/pavleralic/ActionSheetPeerTableViewCellItem"
+  spec.license      = { :type => "MIT", :file => "LICENSE" }
+  spec.author       = { "pavleralic" => "pavle.ralic@gmail.com" }
+
+  spec.ios.deployment_target = "11.0"
+  spec.swift_version = "5.0"
+
+  spec.source        = { :git => "https://github.com/pavleralic/ActionSheetPeerTableViewCellItem.git", :tag => "#{spec.version}" }
+  spec.source_files  = "ActionSheetPeerTableViewCellItem/**/*.{h,m,swift}"
+
+end
--- ActionSheetPeerTableViewCellItemTests/ActionSheetPeerTableViewCellItemTests.swift
+++ ActionSheetPeerTableViewCellItemTests/ActionSheetPeerTableViewCellItemTests.swift
@@ -21,24 +21,28 @@ class ActionSheetPeerTableViewCellItemTests: XCTestCase {
         XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
     }
     
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
+    }
+    
 //    override func setUp() {
 //        // Put setup code here. This method is called before the invocation of each test method in the class.
 //    }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
